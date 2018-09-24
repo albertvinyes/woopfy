1f09077c6f14ad35dd506ebbd4405f2abd710537
albertv <albert.vines@gmail.com>
2018-09-24 20:19:20 +0200

    Wrapping database query in try/except

--- project/accounts/auth.py
+++ project/accounts/auth.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.backends import ModelBackend
 from .models import User, RegularUser
+#from django.shortcuts import get_object_or_404
 
 class AdminOrRegularUserBackend(ModelBackend):
     """
@@ -10,13 +11,14 @@ class AdminOrRegularUserBackend(ModelBackend):
     def authenticate(self, *args, **kwargs):
         id = kwargs.get('username')
         passwd  = kwargs.get('password')
-        user = RegularUser.objects.get(email=id)
-        if user.check_password(passwd):
-            # Yes? return the Django user object
-            return user
-        else:
-            # No? return None - triggers default login failed
-            return None
+        try:
+            user = RegularUser.objects.get(pk=id)
+            if user.check_password(passwd):
+                # Yes? return the Django user object
+                return user
+        except:
+            user = None
+        #comment = get_object_or_404(Comment, pk=comment_id)
         return user
 
     def get_user(self, user_id):
