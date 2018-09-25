d486337f83c182d886405569b131c60e08639ec5
albertv <albert.vines@gmail.com>
2018-09-25 15:30:51 +0200

    Workaround: registering models to admin dashboard here

--- project/accounts/admin.py
+++ project/accounts/admin.py
@@ -3,10 +3,10 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
-
 from .forms import UserAdminCreationForm, UserAdminChangeForm
 from .models import User, RegularUser
+from activities.models import Activity
+from adventures.models import Adventure
 
 class UserAdmin(BaseUserAdmin):
     # The forms to add and change user instances
@@ -36,8 +36,10 @@ class UserAdmin(BaseUserAdmin):
     filter_horizontal = ()
 
 admin.site.site_header = 'Woopfy administration'
-admin.site.register(User, UserAdmin)
+#admin.site.register(User, UserAdmin)
 admin.site.register(RegularUser)
+admin.site.register(Activity)
+admin.site.register(Adventure)
 
 # Remove Group Model from admin. We're not using it.
 admin.site.unregister(Group)
