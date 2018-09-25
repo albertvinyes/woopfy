e4e125a9f77257a35837f60d13a03d033f70f37e
albertv <albert.vines@gmail.com>
2018-09-25 15:12:25 +0200

    Reworked Auth Endpoint

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -6,6 +6,7 @@ from accounts import views
 urlpatterns = [
     path('', views.RegularUserList.as_view()),
     path('<int:pk>/', views.RegularUserDetail.as_view()),
+    path('login/', views.login_view),
 #    path('logout/', views.logout_view),
 #    path('rest-auth/', include('rest_auth.urls')),
     path('test/login/', views.test_login),
--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout
+from django.shortcuts import redirect,render
 from accounts.models import RegularUser
 from accounts.serializers import RegularUserSerializer
+from .auth import AdminOrRegularUserBackend
 from rest_framework import generics
 
 class RegularUserList(generics.ListCreateAPIView):
@@ -13,12 +17,33 @@ class RegularUserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = RegularUser.objects.all()
     serializer_class = RegularUserSerializer
 
+def validateEmail( email ):
+    try:
+        validate_email( email )
+        return True
+    except ValidationError:
+        return False
+
 def login_view(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
+    if request.method == 'POST':
+        logout(request)
+        username = request.POST['email']
+        password = request.POST['password']
+        email_is_valid = validateEmail(username)
+        if not email_is_valid:
+            return HttpResponse("Invalid email")
+        try:
+            user = AdminOrRegularUserBackend.authenticate(request, username=username, password=password)
+            if user:
+                return HttpResponse("User is logged in")
+            else:
+                return HttpResponse(status=401)
+        except:
+            return HttpResponse(status=401)
+    elif request.method == 'GET':
+        return render(request, 'registration/login.html')
+    else:
+        return HttpResponse(status=405)
 
 def logout_view(request):
     logout(request)
