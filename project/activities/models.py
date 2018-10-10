67d12b46f65152c0a242d3999b5093049571097c
albertv <albert.vines@gmail.com>
2018-10-10 23:23:57 +0200

    Updated models

--- project/activities/models.py
+++ project/activities/models.py
@@ -4,4 +4,7 @@ from adventures.models import Adventure
 # Create your models here.
 class Activity(models.Model):
     name = models.CharField(max_length=32)
+    description = models.CharField(max_length=256)
+    location = models.CharField(max_length=256)
+    start = models.DateTimeField('date and time start')
     adventure = models.ForeignKey(Adventure, on_delete=models.CASCADE)
--- project/adventures/models.py
+++ project/adventures/models.py
@@ -1,9 +1,13 @@
+import datetime
 from django.db import models
 
 # Create your models here.
 class Adventure(models.Model):
+    # TODO: remove optional flags
+    today = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
     name = models.CharField(max_length=32)
-    description = models.CharField(max_length=256)
+    description = models.CharField(max_length=256, default="Description")
     location = models.CharField(max_length=256)
-    date_start = models.DateField('date start')
-    date_end = models.DateField('date end')
+    date_start = models.DateField('date start', blank=True, default="2018-12-24")
+    date_end = models.DateField('date end', blank=True, default="2018-12-24")
+    closed = models.BooleanField(default=False)
