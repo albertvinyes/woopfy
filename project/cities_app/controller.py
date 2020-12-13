1a0e527ebd210ae6458e9437dfcb588c21aa9ac2
albertv <albert.vines@gmail.com>
2020-12-13 11:36:13 +0100

    Fixing Cities app order by issue

--- project/cities_app/controller.py
+++ project/cities_app/controller.py
@@ -6,10 +6,10 @@ class CityController():
     # Return matching cities according to a start string
     def get_cities_starting_with(lookup_str, verbose):
         # name__istartswith is case insensitive, it matches when the city name starts with the sring
-        query = City.objects.filter(name__istartswith=lookup_str)
+        query = City.objects.filter(name__istartswith=lookup_str).order_by('-population')
         # fields we want to retrieve from the city
         if (not verbose):
-            fields = ('name','display_name', 'population').order_by('-population')
+            fields = ('name','display_name', 'population')
             serialized_query = serialize('json', query, fields=fields)
         else:
             serialized_query = serialize('json', query)
